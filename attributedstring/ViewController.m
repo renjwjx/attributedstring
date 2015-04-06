4b554cfb2bd91a89138d354bc9e4b7fb56b10f3a
renjwjx <renjinwei@renjinweideMac-mini.local>
2015-04-06 20:40:02 +0800

    init commit

--- attributedstring/ViewController.m
+++ attributedstring/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelAttributedString;
+//@property (weak, nonatomic) IBOutlet UITextView *textviewAttributedString;
 
 @end
 
@@ -16,7 +18,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *strLink = @"www.mi.com testalkdsfiasfleiefeilfsdfsdlfldfdfd \n open link";
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *attribString = [[NSMutableAttributedString alloc] initWithString:strLink];
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
+    [attribString beginEditing];
+    [attribString addAttribute:NSLinkAttributeName value:url range:NSMakeRange(30, strLink.length - 30)];
+    [attribString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"Courier" size:28.0] range:NSMakeRange(0, strLink.length)];
+    [attribString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(10, strLink.length - 10)];
+    [attribString addAttribute:NSUnderlineStyleAttributeName value:[NSNumber numberWithInteger:NSUnderlineStyleSingle] range:NSMakeRange(20, strLink.length - 20)];
+    
+    [attribString endEditing];
+    UITextView *_textviewAttributedString = [[UITextView alloc] initWithFrame:CGRectMake(0, 50, self.view.frame.size.width * 0.8, self.view.frame.size.height * 0.8)];
+    
+    _labelAttributedString.attributedText = attribString;
+    _labelAttributedString.numberOfLines = 3;
+    _textviewAttributedString.center = self.view.center;
+    NSLog(@"%f, %f", self.view.center.x, self.view.center.y);
+    _textviewAttributedString.attributedText = attribString;
+    _textviewAttributedString.editable = NO;
+
+    NSDictionary *linkAttributes = @{NSForegroundColorAttributeName: [UIColor blackColor],
+                                     NSUnderlineColorAttributeName: [UIColor blackColor]
+                                     };
+    
+    _textviewAttributedString.linkTextAttributes = linkAttributes;
+    _textviewAttributedString.delegate = self;
+    [self.view addSubview:_textviewAttributedString];
 }
 
 - (void)didReceiveMemoryWarning {
